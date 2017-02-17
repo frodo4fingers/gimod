47a26b2d45ead90576bdffe4d76f778de2096b7a
friedrich <f.dinsel@gmail.com>
2017-02-17 22:42:33 +0100

    moved graphical content

---
+++ mpl/__init__.py
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+""" contains the classes responsible for moving matplotlib stuff """
+
+from .spanCircle import SpanCircle
+from .spanLine import SpanLine
+from .spanRectangle import SpanRectangle
+from .spanWorld import SpanWorld
+
+from .draggablePoint import DraggablePoint
