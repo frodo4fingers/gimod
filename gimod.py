ffc18276a31811bf3230ad1c3b06b602304ecf44
frodo4fingers <f.dinsel@gmail.com>
2017-09-01 20:11:53 +0200

    extracted the mesh functionalities to MeshOptions

--- gimod.py
+++ gimod.py
@@ -19,7 +19,7 @@ from core import Builder, ImageTools
 from gui import InfoTree, MeshOptions, PlotWidget, PolyToolBar
 # from mpl import Helper
 
-# TODO: CLEAAAAAAAAAAAAAN THIS!!!!
+# TODO: move meshing methods to MeshOptions
 
 
 class GIMod(QMainWindow):
@@ -121,99 +121,6 @@ class GIMod(QMainWindow):
         with open('version.json') as v:
             content = v.read()
         QMessageBox.information(self, "About", content)
-
-    def changedChbxMeshRefine(self):
-        if self.chbx_mesh_refine.isChecked() is True:
-            self.cbx_mesh_refine.setEnabled(True)
-            self.mesh_refine = True
-        else:
-            self.cbx_mesh_refine.setEnabled(False)
-            self.mesh_refine = False
-
-    def changedChbxSmooth(self):
-        if self.chbx_smooth.isChecked() is True:
-            self.cbx_smooth.setEnabled(True)
-            self.spb_smooth.setEnabled(True)
-            # self.smooth = True
-            # self.cbx_smooth = int(self.cbx_smooth.currentText())
-            # self.spb_smooth = self.spb_smooth.value()
-            # print("%i, %i" % (self.cbx_smooth, self.spb_smooth))
-        else:
-            self.cbx_smooth.setEnabled(False)
-            self.spb_smooth.setEnabled(False)
-            # self.smooth = True
-            # self.cbx_smooth = None
-            # self.spb_smooth = None
-            # print("%i, %i" % (self.cbx_smooth, self.spb_smooth))
-
-    def changedChbxSwitches(self):
-        if self.chbx_switches.isChecked() is True:
-            self.le_switches.setEnabled(True)
-        else:
-            self.le_switches.setEnabled(False)
-
-    def clickedBtnMesh(self):
-        if self.mesh_refine is False:
-            self.refine_method = None
-        elif self.mesh_refine is True and self.cbx_mesh_refine.currentText() == "quadratic":
-            self.refine_method = "createP2"
-        elif self.mesh_refine is True and self.cbx_mesh_refine.currentText() == "spatially":
-            self.refine_method = "createH2"
-
-        if self.chbx_smooth.isChecked() is False:
-            self.smooth_method = None
-        else:
-            self.smooth_method = [
-                int(self.cbx_smooth.currentText()), self.spb_smooth.value()]
-
-        if self.chbx_switches.isChecked() is False:
-            self.switches = None
-        else:
-            self.switches = self.le_switches.text()
-            # TODO make th switches work -->
-            # http://pygimli.org/_examples_auto/modelling/plot_hybrid-mesh-2d.html?highlight=switches
-
-        self.statusBar.showMessage("generating mesh...")
-        self.mesh = createMesh(self.builder.getPoly(), quality=self.spb_mesh_quality.value(
-        ), area=self.spb_cell_area.value(), smooth=self.smooth_method, switches=self.switches)
-
-        if self.mesh_refine is True and self.cbx_mesh_refine.currentText() == "quadratic":
-            self.statusBar.showMessage("create quadratic...")
-            self.mesh = self.mesh.createP2()
-        elif self.mesh_refine is True and self.cbx_mesh_refine.currentText() == "spatially":
-            self.statusBar.showMessage("create spatially...")
-            self.mesh = self.mesh.createH2()
-
-        self.statusBar.showMessage(str(self.mesh))
-        self.btn_mesh_export.setEnabled(True)
-        self.showMesh()
-
-    def showMesh(self):
-        self.plotWidget.axis.cla()
-        if self.chbx_mesh_attr.isChecked() is True:
-            self.regionGetAttributes()
-            pg.show(self.mesh, pg.solver.parseArgToArray(self.attr_map, self.mesh.cellCount(
-            ), self.mesh), ax=self.plotWidget.axis)
-            pg.show(drawMeshBoundaries(self.plotWidget.axis, self.mesh,
-                                       hideMesh=False), ax=self.plotWidget.axis, fillRegion=False)
-        else:
-            pg.show(self.mesh, ax=self.plotWidget.axis)
-
-        self.plotWidget.axis.set_ylim(self.plotWidget.axis.get_ylim()[::-1])
-        self.plotWidget.canvas.draw()
-
-    def meshExport(self):
-        """
-            export the final mesh
-        """
-        export_mesh = QFileDialog.getSaveFileName(
-            self, caption="Save Mesh")
-
-        # if export_poly:
-        if export_mesh.endswith(".bms"):
-            writePLC(self.mesh, export_mesh)
-        else:
-            writePLC(self.mesh, export_mesh + ".bms")
 
 
 if __name__ == "__main__":
--- gui/meshWidget.py
+++ gui/meshWidget.py
@@ -27,6 +27,7 @@ class MeshOptions(QWidget):
 
     def setupWidget(self):
         """Design the layout of the tab that holds the options for tetgen."""
+        # define the mesh quality regarding the inner angle in a cell
         la_mesh_quality = QLabel("Mesh Quality:")
         self.spb_mesh_quality = QDoubleSpinBox(self)
         self.spb_mesh_quality.setMinimum(30.0)
@@ -34,12 +35,14 @@ class MeshOptions(QWidget):
         self.spb_mesh_quality.setValue(30.0)
         self.spb_mesh_quality.setSingleStep(0.1)
 
-        self.la_cell_area = QLabel("max. cell area:")
+        # define the maximum area a cell can adjust to
+        la_cell_area = QLabel("max. cell area:")
         self.spb_cell_area = QDoubleSpinBox(self)
         self.spb_cell_area.setValue(0.0)
         self.spb_cell_area.setSingleStep(0.01)
 
-        self.la_mesh_refine = QLabel("Refinement:")
+        # define the box to choose which refinement algorithm might be chosen
+        la_mesh_refine = QLabel("Refinement:")
         self.cbx_mesh_refine = QComboBox()
         self.cbx_mesh_refine.setSizePolicy(
             QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -47,16 +50,18 @@ class MeshOptions(QWidget):
         self.chbx_mesh_refine = QCheckBox()
         self.cbx_mesh_refine.addItem("quadratic")
         self.cbx_mesh_refine.addItem("spatially")
+        # REVIEW: whats this?!
         self.mesh_refine = False
 
-        self.la_smooth = QLabel("Smooth:")
+        # define the smoothness variable and the number of iterations
+        la_smooth = QLabel("Smooth:")
         self.chbx_smooth = QCheckBox()
         self.cbx_smooth = QComboBox()
         self.cbx_smooth.setSizePolicy(
             QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.cbx_smooth.setToolTip("1 node center\n2 weighted node center")
         self.cbx_smooth.setEnabled(False)
-        # self.cbx_smooth.addItem("0")
+        # TODO: replace 1 and 2 with the thing they're representing, sending the algorithm by number later on
         self.cbx_smooth.addItem("1")
         self.cbx_smooth.addItem("2")
         self.spb_smooth = QSpinBox()
@@ -65,49 +70,62 @@ class MeshOptions(QWidget):
         self.spb_smooth.setMinimum(1)
         self.spb_smooth.setValue(5)
 
+        # define the set of switches to override the other adjustments and
+        # access tetgen directly through the commandline options
         self.la_switches = QLabel("Switches:")
         self.chbx_switches = QCheckBox()
         self.le_switches = QLineEdit("-pzeAfaq31")
         self.le_switches.setEnabled(False)
         self.switches = None
 
-        self.la_mesh_show_attr = QLabel("Show Attributes:")
+        # allow colored attribute table overlay together with the mesh
+        la_mesh_show_attr = QLabel("Show Attributes:")
         self.chbx_mesh_attr = QCheckBox()
 
+        # the button to hit and run
         self.btn_mesh = QPushButton("mesh")
         self.btn_mesh.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        # define a button to export the just generated mesh
         self.btn_mesh_export = QPushButton()
         self.btn_mesh_export.setToolTip("save as *.bms")
         self.btn_mesh_export.setIcon(QIcon("icons/ic_save_black_24px.svg"))
         self.btn_mesh_export.setSizePolicy(
             QSizePolicy.Minimum, QSizePolicy.Minimum)
         self.btn_mesh_export.setEnabled(False)
-        # labels stacked in vbox
+
+        # stack the labels in a vertical layout
         vbox_mesh_labels = QVBoxLayout()
         vbox_mesh_labels.addWidget(la_mesh_quality)
-        vbox_mesh_labels.addWidget(self.la_cell_area)
-        vbox_mesh_labels.addWidget(self.la_mesh_refine)
-        vbox_mesh_labels.addWidget(self.la_smooth)
+        vbox_mesh_labels.addWidget(la_cell_area)
+        vbox_mesh_labels.addWidget(la_mesh_refine)
+        vbox_mesh_labels.addWidget(la_smooth)
         vbox_mesh_labels.addWidget(self.la_switches)
-        vbox_mesh_labels.addWidget(self.la_mesh_show_attr)
-        # rest organized in layout boxes
+        vbox_mesh_labels.addWidget(la_mesh_show_attr)
+
+        # stack the widgets also vertical so it looks nice and clean
         vbox_mesh_params = QVBoxLayout()
         vbox_mesh_params.addWidget(self.spb_mesh_quality)
         vbox_mesh_params.addWidget(self.spb_cell_area)
+
         hbox_mesh_refine = QHBoxLayout()
         hbox_mesh_refine.addWidget(self.chbx_mesh_refine)
         hbox_mesh_refine.addWidget(self.cbx_mesh_refine)
         vbox_mesh_params.addLayout(hbox_mesh_refine)
+
         hbox_mesh_smooth = QHBoxLayout()
         hbox_mesh_smooth.addWidget(self.chbx_smooth)
         hbox_mesh_smooth.addWidget(self.cbx_smooth)
         hbox_mesh_smooth.addWidget(self.spb_smooth)
         vbox_mesh_params.addLayout(hbox_mesh_smooth)
-        # TODO
-        # hbox_mesh_switches = QHBoxLayout()
-        # hbox_mesh_switches.addWidget(self.chbx_switches)
-        # hbox_mesh_switches.addWidget(self.le_switches)
-        # vbox_mesh_params.addLayout(hbox_mesh_switches)
+
+        # TODO: make the label auto adjusted to the changes from the widgets
+        # TODO:disable all widgets if one wants to set the options by hand
+        hbox_mesh_switches = QHBoxLayout()
+        hbox_mesh_switches.addWidget(self.chbx_switches)
+        hbox_mesh_switches.addWidget(self.le_switches)
+        vbox_mesh_params.addLayout(hbox_mesh_switches)
+
         hbox_mesh_attr = QHBoxLayout()
         hbox_mesh_attr.addWidget(self.chbx_mesh_attr)
         hbox_mesh_attr.addStretch(1)
@@ -127,3 +145,96 @@ class MeshOptions(QWidget):
         vbox_mesh.addStretch(1)
 
         self.setLayout(vbox_mesh)
+
+    def changedChbxMeshRefine(self):
+        if self.chbx_mesh_refine.isChecked() is True:
+            self.cbx_mesh_refine.setEnabled(True)
+            self.mesh_refine = True
+        else:
+            self.cbx_mesh_refine.setEnabled(False)
+            self.mesh_refine = False
+
+    def changedChbxSmooth(self):
+        if self.chbx_smooth.isChecked() is True:
+            self.cbx_smooth.setEnabled(True)
+            self.spb_smooth.setEnabled(True)
+            # self.smooth = True
+            # self.cbx_smooth = int(self.cbx_smooth.currentText())
+            # self.spb_smooth = self.spb_smooth.value()
+            # print("%i, %i" % (self.cbx_smooth, self.spb_smooth))
+        else:
+            self.cbx_smooth.setEnabled(False)
+            self.spb_smooth.setEnabled(False)
+            # self.smooth = True
+            # self.cbx_smooth = None
+            # self.spb_smooth = None
+            # print("%i, %i" % (self.cbx_smooth, self.spb_smooth))
+
+    def changedChbxSwitches(self):
+        if self.chbx_switches.isChecked() is True:
+            self.le_switches.setEnabled(True)
+        else:
+            self.le_switches.setEnabled(False)
+
+    def clickedBtnMesh(self):
+        if self.mesh_refine is False:
+            self.refine_method = None
+        elif self.mesh_refine is True and self.cbx_mesh_refine.currentText() == "quadratic":
+            self.refine_method = "createP2"
+        elif self.mesh_refine is True and self.cbx_mesh_refine.currentText() == "spatially":
+            self.refine_method = "createH2"
+
+        if self.chbx_smooth.isChecked() is False:
+            self.smooth_method = None
+        else:
+            self.smooth_method = [
+                int(self.cbx_smooth.currentText()), self.spb_smooth.value()]
+
+        if self.chbx_switches.isChecked() is False:
+            self.switches = None
+        else:
+            self.switches = self.le_switches.text()
+            # TODO make th switches work -->
+            # http://pygimli.org/_examples_auto/modelling/plot_hybrid-mesh-2d.html?highlight=switches
+
+        self.statusBar.showMessage("generating mesh...")
+        self.mesh = createMesh(self.builder.getPoly(), quality=self.spb_mesh_quality.value(
+        ), area=self.spb_cell_area.value(), smooth=self.smooth_method, switches=self.switches)
+
+        if self.mesh_refine is True and self.cbx_mesh_refine.currentText() == "quadratic":
+            self.statusBar.showMessage("create quadratic...")
+            self.mesh = self.mesh.createP2()
+        elif self.mesh_refine is True and self.cbx_mesh_refine.currentText() == "spatially":
+            self.statusBar.showMessage("create spatially...")
+            self.mesh = self.mesh.createH2()
+
+        self.statusBar.showMessage(str(self.mesh))
+        self.btn_mesh_export.setEnabled(True)
+        self.showMesh()
+
+    def showMesh(self):
+        self.plotWidget.axis.cla()
+        if self.chbx_mesh_attr.isChecked() is True:
+            self.regionGetAttributes()
+            pg.show(self.mesh, pg.solver.parseArgToArray(self.attr_map, self.mesh.cellCount(
+            ), self.mesh), ax=self.plotWidget.axis)
+            pg.show(drawMeshBoundaries(self.plotWidget.axis, self.mesh,
+                                       hideMesh=False), ax=self.plotWidget.axis, fillRegion=False)
+        else:
+            pg.show(self.mesh, ax=self.plotWidget.axis)
+
+        self.plotWidget.axis.set_ylim(self.plotWidget.axis.get_ylim()[::-1])
+        self.plotWidget.canvas.draw()
+
+    def meshExport(self):
+        """
+            export the final mesh
+        """
+        export_mesh = QFileDialog.getSaveFileName(
+            self, caption="Save Mesh")
+
+        # if export_poly:
+        if export_mesh.endswith(".bms"):
+            writePLC(self.mesh, export_mesh)
+        else:
+            writePLC(self.mesh, export_mesh + ".bms")
