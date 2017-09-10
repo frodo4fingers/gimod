127ad3a9878822328215f2bac57b8b5e89f96ad5
frodo4fingers <f.dinsel@gmail.com>
2017-09-10 10:42:59 +0200

    commented all stuff

--- gui/polyToolBar.py
+++ gui/polyToolBar.py
@@ -17,65 +17,65 @@ class PolyToolBar(QToolBar):
     """
 
     def __init__(self, parent=None):
+        """
+        Initialize the widget itself and call super class to get the functionality of QToolBar.
+
+        Parameters
+        ----------
+        parent: <__main__.GIMod object>
+            Every widget that needs to be accessed is called in :class:`~GIMod`
+        """
         super(PolyToolBar, self).__init__(parent)
         self.parent = parent
 
         self.setupTools()
 
-        # connect actiosn to their functions
-        self.acn_image.triggered.connect(self.parent.image_tools.imagery)
-        self.acn_imageAsBackground.stateChanged.connect(self.parent.image_tools.imageryBackground)
-        self.acn_imageThreshold1.valueChanged.connect(self.parent.image_tools.updateImagery)
-        self.acn_imageThreshold2.valueChanged.connect(self.parent.image_tools.updateImagery)
-        self.acn_imageDensity.valueChanged.connect(self.parent.image_tools.updateImagery)
-        self.acn_imagePolys.valueChanged.connect(self.parent.image_tools.polysFromImage)
-
-        self.acn_polygonize.triggered.connect(self.parent.builder.formPolygonFromFigure)
-
-        self.acn_reset_figure.triggered.connect(self.parent.builder.resetFigure)
-
-        self.acn_world.triggered.connect(self.parent.builder.formPolyWorld)
-        self.acn_rectangle.triggered.connect(self.parent.builder.formPolyRectangle)
-        self.acn_circle.triggered.connect(self.parent.builder.formPolyCircle)
-        self.acn_line.triggered.connect(self.parent.builder.formPolyLine)
-        self.acn_polygon.triggered.connect(self.parent.builder.formPolygon)
-        self.acn_markerCheck.triggered.connect(self.parent.builder.markersMove)
-
-        self.acn_gridToggle.triggered.connect(self.parent.builder.toggleGrid)
-        self.acn_magnetizeGrid.triggered.connect(self.parent.builder.magnetizeGrid)
-        self.acn_magnetizePoly.triggered.connect(self.parent.builder.magnetizePoly)
-
     def setupTools(self):
         """Implement functionality to the toolbar."""
-
+        # create a group for the image tools
         self.grp_imageTools = QActionGroup(self)
         self.acn_image = QAction(QIcon('icons/ic_image.svg'), 'image', self.grp_imageTools, checkable=True)
         self.acn_image.setToolTip("Load image to set as model background or try to extract polygons from")
 
+        # action to reset the figure and delete all created content
         self.acn_reset_figure = QAction(QIcon('icons/ic_reset.svg'), 'image', self.grp_imageTools)
         self.acn_reset_figure.setToolTip("Reset everything to nothing")
         self.acn_reset_figure.setEnabled(False)
 
+        # create a real polygon (plc) if everything is ready
         self.acn_polygonize = QAction(QIcon('icons/ic_polygonize.svg'), 'image', self.grp_imageTools)
         self.acn_polygonize.setToolTip("polygonize the contours")
         self.acn_polygonize.setEnabled(False)
 
+        # set the image as background for drawing trace
         self.acn_imageAsBackground = QCheckBox('as background')
         # self.acn_imageAsBackground.setEnabled(False)
         self.acn_imageAsBackground.setToolTip("set the chosen image as background to paint your model")
+
+        # the lower bound for the b/w threshold
         self.acn_imageThreshold1 = QSpinBox()
         self.acn_imageThreshold1.setRange(0, 254)
         self.acn_imageThreshold1.setValue(200)
         self.acn_imageThreshold1.setToolTip("bottom value for threshold")
+
+        # the upper bound for the b/w threshold
         self.acn_imageThreshold2 = QSpinBox()
         self.acn_imageThreshold2.setRange(1, 255)
         self.acn_imageThreshold2.setValue(255)
         self.acn_imageThreshold2.setToolTip("top value for threshold")
+
+        # the point density of each contour
+        # this sets number of points that are being skipped in the path
         self.acn_imageDensity = QSpinBox()
         self.acn_imageDensity.setRange(1, 10)
         self.acn_imageDensity.setToolTip("set density of dots in polygon")
+
+        # the polygons are ordered by area... so this allows to take one by one
+        # into account getting smaller each step
         self.acn_imagePolys = QSpinBox()
         self.acn_imagePolys.setToolTip("set the number of polygons used for model creation")
+
+        # sort the widgets nicely in a layout
         acnBox = QHBoxLayout()
         acnBox.addWidget(self.acn_imageAsBackground)
         acnBox.addWidget(self.acn_imageThreshold1)
@@ -86,43 +86,55 @@ class PolyToolBar(QToolBar):
         acnWidget = QWidget()
         acnWidget.setLayout(acnBox)
 
+        # next group for the polyttols of GIMLi
         self.grp_polyTools = QActionGroup(self)
+        # the button that allows to access the drawing of the world
         self.acn_world = QAction(QIcon('icons/ic_spanWorld.svg'), 'world', self.grp_polyTools, checkable=True)
         self.acn_world.setToolTip("Create your model world where everything happens")
 
+
+        # the button that allows to access the drawing of a rectangle
         self.acn_rectangle = QAction(QIcon('icons/ic_spanRectangle.svg'), 'rectangle', self.grp_polyTools, checkable=True)
         self.acn_rectangle.setToolTip("Create a rectangle body")
         self.acn_rectangle.setEnabled(False)
 
+        # the button that allows to access the drawing of a circle
         self.acn_circle = QAction(QIcon('icons/ic_spanCircle.svg'), 'circle', self.grp_polyTools, checkable=True)
         self.acn_circle.setToolTip("Create a circle body")
         self.acn_circle.setEnabled(False)
 
+        # the button that allows to access the drawing of a line
         self.acn_line = QAction(QIcon('icons/ic_spanLine.png'), 'line', self.grp_polyTools, checkable=True)
         self.acn_line.setToolTip("Create a line by clicking")
         self.acn_line.setEnabled(False)
 
+        # the button that allows to access the drawing of a random polygon
         self.acn_polygon = QAction(QIcon('icons/ic_spanPoly.svg'), 'polygon', self.grp_polyTools, checkable=True)
         self.acn_polygon.setToolTip("Create a polygon by clicking, finish with double click")
         self.acn_polygon.setEnabled(False)
 
+        # action to marke each region's marker position.. those dots can be
+        # dragged and will be checked for new positions
         self.acn_markerCheck = QAction(QIcon('icons/marker_check.svg'), 'marker', self.grp_polyTools, checkable=True)
         self.acn_markerCheck.setToolTip("check and reset marker positions")
         self.acn_markerCheck.setEnabled(False)
 
-        # self.grp_gridTools = QActionGroup(self)
+        # action to turn on/off a grid. should help while creating the model
         self.acn_gridToggle = QAction(QIcon('icons/grid.svg'), 'grid', None, checkable=True)
         self.acn_gridToggle.setToolTip("turn on and off a grid")
         self.acn_gridToggle.setEnabled(False)
 
+        # action to magnetize all grid lines
         self.acn_magnetizeGrid = QAction(QIcon('icons/grid_magnetize.svg'), 'magnetizeGrid', None, checkable=True)
         self.acn_magnetizeGrid.setToolTip("magnetize the grid junctions")
         self.acn_magnetizeGrid.setEnabled(False)
 
+        # action to magnetize the vertices/nodes of every polygon
         self.acn_magnetizePoly = QAction(QIcon('icons/magnetize.svg'), 'magnetizePoly', None, checkable=True)
         self.acn_magnetizePoly.setToolTip("magnetize the polygons")
         self.acn_magnetizePoly.setEnabled(False)
 
+        # add all actions and widgets to the toolbar
         self.addAction(self.acn_image)
         self.widgetAction = self.addWidget(acnWidget)
         self.widgetAction.setVisible(False)
@@ -142,4 +154,5 @@ class PolyToolBar(QToolBar):
         self.addAction(self.acn_magnetizeGrid)
         self.addAction(self.acn_magnetizePoly)
 
+        # set a iconsize!
         self.setIconSize(QSize(18, 18))
