2e9ee27e3a1018ec2fce69009b2254632d9132a2
frodo4fingers <f.dinsel@gmail.com>
2017-09-10 10:27:18 +0200

    changed variable name of statusbar and moved save functions to menubar

--- gimod.py
+++ gimod.py
@@ -2,17 +2,17 @@
 # encoding: UTF-8
 
 try:
-    from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLabel, QSpinBox, QDoubleSpinBox, QComboBox, QSizePolicy, QCheckBox, QLineEdit,  QPushButton, QStatusBar, QToolBar, QTabWidget, QSplitter, QAction, QMessageBox
+    from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLabel, QSpinBox, QDoubleSpinBox, QComboBox, QSizePolicy, QCheckBox, QLineEdit,  QPushButton, QStatusBar, QToolBar, QTabWidget, QSplitter, QAction, QMessageBox, QFileDialog, QMenu
     from PyQt5.QtCore import QSize, Qt
     from PyQt5.QtGui import QIcon
 
 except ImportError:
-    from PyQt4.QtGui import QMainWindow, QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLabel, QSpinBox, QDoubleSpinBox, QComboBox, QSizePolicy, QCheckBox, QLineEdit,  QPushButton, QStatusBar, QToolBar, QTabWidget, QSplitter, QAction, QMessageBox, QIcon
+    from PyQt4.QtGui import QMainWindow, QWidget, QApplication, QVBoxLayout, QHBoxLayout, QLabel, QSpinBox, QDoubleSpinBox, QComboBox, QSizePolicy, QCheckBox, QLineEdit,  QPushButton, QStatusBar, QToolBar, QTabWidget, QSplitter, QAction, QMessageBox, QIcon, QFileDialog, QMenu
     from PyQt4.QtCore import QSize, Qt
 
 import sys
 import pygimli as pg
-from pygimli.meshtools import createMesh, writePLC
+from pygimli.meshtools import createMesh, exportPLC, exportFenicsHDF5Mesh
 from pygimli.mplviewer import drawMeshBoundaries, drawMesh, drawPLC, drawModel
 
 from core import Builder, ImageTools
@@ -27,7 +27,7 @@ class GIMod(QMainWindow):
         self.image_tools = ImageTools(self)
 
         # menu actions
-        self.acn_aboutVerison.triggered.connect(self.aboutVersion)
+        self.mb_aboutVerison.triggered.connect(self.aboutVersion)
 
         # toolbar actions
         self.toolBar.acn_image.triggered.connect(self.image_tools.imagery)
@@ -52,21 +52,38 @@ class GIMod(QMainWindow):
         self.toolBar.acn_magnetizeGrid.triggered.connect(self.builder.magnetizeGrid)
         self.toolBar.acn_magnetizePoly.triggered.connect(self.builder.magnetizePoly)
 
-
         self.info_tree.btn_redraw.clicked.connect(self.info_tree.redrawTable)
         self.info_tree.btn_undo.clicked.connect(self.builder.undoPoly)
         self.info_tree.btn_redo.clicked.connect(self.builder.redoPoly)
+        # self.info_tree.btn_export.clicked.connect(self.exportPoly)
+
+        # mesh/poly export
+        # self.mesh_options.btn_mesh_export.clicked.connect(self.exportMesh)
 
     def initUI(self):
         """Set the GUI together from the other widgets."""
         # instanciate the status bar to prompt some information of what is
         # going on beneath the hood
-        self.statusBar = QStatusBar()
-        self.setStatusBar(self.statusBar)
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
 
         # instanciate the toolbar with the polytool functionality
         self.toolBar = PolyToolBar(self)
         self.addToolBar(self.toolBar)
+
+        self.menubar = self.menuBar()
+        # call the menu generation
+        self.menuBarItems()
+
+        menu_file = self.menubar.addMenu("&File")
+        menu_file_save = QMenu("&Save", self)
+        menu_file_save.addAction(self.mb_save_poly)
+        menu_file_save.addAction(self.mb_save_mesh)
+
+        menu_file.addMenu(menu_file_save)
+
+        menu_about = self.menubar.addMenu("&About")
+        menu_about.addAction(self.mb_aboutVerison)
 
         # initialize the plot widget where everything will be drawn
         self.plotWidget = PlotWidget(self)
@@ -92,12 +109,6 @@ class GIMod(QMainWindow):
         splitter.addWidget(tabBox)
         splitter.addWidget(self.plotWidget)
 
-        self.acn_aboutVerison = QAction("Version", self)
-
-        menubar = self.menuBar()
-        menu_file = menubar.addMenu("&About")
-        menu_file.addAction(self.acn_aboutVerison)
-
         self.setCentralWidget(splitter)
 
         self.setGeometry(200, 100, 1000, 600)
@@ -105,6 +116,20 @@ class GIMod(QMainWindow):
         self.setWindowTitle("GIMod")
         self.setWindowIcon(QIcon('icons/logo.png'))
         self.show()
+
+    def menuBarItems(self):
+        """Create all entries visible in the menubar and submenus."""
+        # instanciate entries for "About"
+        self.mb_aboutVerison = QAction("Version", self)
+        # instanciate entries for "File"
+        # action to save a poly figure
+        self.mb_save_poly = QAction(QIcon('icons/ic_save.svg'), '&Poly', self)
+        self.mb_save_poly.setStatusTip("Save the created polygon file to a plc")
+        self.mb_save_poly.setEnabled(False)
+        # action to save a mesh
+        self.mb_save_mesh = QAction(QIcon('icons/ic_save.svg'), '&Mesh', self)
+        self.mb_save_mesh.setStatusTip("Save the generated mesh file")
+        self.mb_save_mesh.setEnabled(False)
 
     def aboutVersion(self):
         with open('version.json') as v:
@@ -114,13 +139,26 @@ class GIMod(QMainWindow):
     def exportPoly(self):
         """Export the poly figure."""
         export_poly = QFileDialog.getSaveFileName(
-            self, caption='Save Poly Figure')
+            self, caption='Save Poly Figure')[0]
 
         # if export_poly:
         if export_poly.endswith('.poly'):
-            writePLC(self.poly, export_poly)
+            exportPLC(self.builder.poly, export_poly)
         else:
-            writePLC(self.poly, export_poly + '.poly')
+            exportPLC(self.builder.poly, export_poly + '.poly')
+
+    def exportMesh(self):
+        """
+            export the final mesh
+        """
+        export_mesh = QFileDialog.getSaveFileName(
+            self, caption="Save Mesh")[0]
+        print(export_mesh)
+        # if export_poly:
+        if export_mesh.endswith(".bms"):
+            exportFenicsHDF5Mesh(self.mesh_options.mesh, export_mesh)
+        else:
+            exportFenicsHDF5Mesh(self.mesh_options.mesh, export_mesh + ".bms")
 
 
 if __name__ == "__main__":
--- mpl/magnetizePolys.py
+++ mpl/magnetizePolys.py
@@ -14,7 +14,7 @@ class MagnetizePolygons():
     def __init__(self, parent, x, y):
         self.figure = parent.figure
         self.span = parent.span
-        self.statusBar = parent.statusBar
+        self.statusBar = parent.statusbar
         self.x = x
         self.y = y
         line, = self.figure.axis.plot([0], [0], 'o', c='#ff0000')
